b708404199fca1523bae5e5a2b96ef99b5efedc9
Weston Gorczyca <weston.gorczyca@gmail.com>
2026-07-08 20:29:06 -0700

    okie dokie

---
+++ AluminiumCLI/cmd/aluminium/main.go
@@ -0,0 +1,11 @@
+// main.go
+package main
+
+import (
+	// Your project's core command package
+	"github.com/PandaTwoxx/Aluminium/cmd" 
+)
+
+func main() {
+	cmd.Execute() // Calls the function defined in root.go
+}
---
+++ AluminiumCLI/cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "aluminium",
+	Short: "Aluminium is a CLI tool for downloading system packages.",
+	Long:  `Aluminium is a powerful command-line interface (CLI) tool that helps you download and manage system packages efficiently.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("version", "v", false, "Print version number")
+}
